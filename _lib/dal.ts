213b8c3705bbc79ff37ff613193ad20258f66cd2
Humberto Ribeiro <betofoxmal@gmail.com>
2026-02-25 23:35:05 -0300

    style: return types user

--- _lib/dal.ts
+++ _lib/dal.ts
@@ -2,13 +2,14 @@ import 'server-only';
 import { cache } from 'react';
 import { verifySession } from '@/_lib/session';
 import { UserRepository } from '@/_lib/userrepository';
+import { UserDetailsProps } from '@/_types';
 
 export const getUser = cache(async () => {
     const session = await verifySession();
     if (!session) return null;
 
     try {
-        const user = await UserRepository.findPublicById(session.userId);
+        const user: UserDetailsProps = await UserRepository.findPublicById(session.userId);
 
         if (!user) return null;
 
