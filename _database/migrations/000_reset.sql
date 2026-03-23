e47edf6dc4f312b8e04fb47730bb0a2788826898
Humberto Ribeiro <betofoxmal@gmail.com>
2026-03-23 03:52:32 -0300

    fix: update reset data base

--- _database/migrations/000_reset.sql
+++ _database/migrations/000_reset.sql
@@ -1,3 +1,6 @@
+-- Garante que estamos operando como o owner das tabelas
+SET ROLE NONE;
+
 -- 1. Trigger
 DROP TRIGGER IF EXISTS trigger_update_users_updated_at ON users;
 
@@ -5,24 +8,31 @@ DROP TRIGGER IF EXISTS trigger_update_users_updated_at ON users;
 DROP FUNCTION IF EXISTS update_updated_at;
 
 -- 3. Views
-DROP VIEW IF EXISTS users_public_active;
-DROP VIEW IF EXISTS users_active;
-DROP VIEW IF EXISTS users_public;
-DROP VIEW IF EXISTS users_admin_public;
+DROP VIEW IF EXISTS users_public_active CASCADE;
+DROP VIEW IF EXISTS users_active CASCADE;
+DROP VIEW IF EXISTS users_public CASCADE;
+DROP VIEW IF EXISTS users_admin_public CASCADE;
 
 -- 4. Tables
-DROP TABLE IF EXISTS verification_tokens;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rate_limits CASCADE;
+DROP TABLE IF EXISTS verification_tokens CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 -- 5. Enum
-DROP TYPE IF EXISTS user_role;
+DROP TYPE IF EXISTS user_role CASCADE;
 
 -- 6. Tabela de migrations
-DROP TABLE IF EXISTS schema_migrations;
+DROP TABLE IF EXISTS schema_migrations CASCADE;
 
 -- 7. Extensões
 DROP EXTENSION IF EXISTS pgcrypto CASCADE;
 DROP EXTENSION IF EXISTS citext CASCADE;
 
 -- 8. Role
-DROP ROLE IF EXISTS app_backend_role;
+DO $$ BEGIN
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app_backend_role') THEN
+        REASSIGN OWNED BY app_backend_role TO CURRENT_USER;
+        DROP OWNED BY app_backend_role;
+        DROP ROLE app_backend_role;
+    END IF;
+END $$;
