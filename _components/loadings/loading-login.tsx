ac37ca6b55a458a05f06ed2ad85209679b431d4d
Humberto Ribeiro <betofoxmal@gmail.com>
2026-03-25 01:19:20 -0300

    style: update centralize

--- _components/loadings/loading-login.tsx
+++ _components/loadings/loading-login.tsx
@@ -6,7 +6,7 @@ export function LoadingLogin() {
     return (
         <div className="w-full 2xl:w-2/4 2xl:p-8">
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-72">
-                <Skeleton className="size-16 2xl:hidden rounded-full" />
+                <Skeleton className="size-16 2xl:hidden rounded-full mx-auto" />
 
                 <div className="grid gap-2">
                     <Skeleton className="h-5 w-44 mx-auto" />
