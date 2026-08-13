a538c7d99d6abdc1639def86a928b0d4535c98a1
Humberto Ribeiro <betofoxmal@gmail.com>
2026-08-12 23:00:54 -0300

    chore: lib shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Pirata_One } from 'next/font/google';
 import '@/app/globals.css';
 import { ThemeProvider } from '@/_components/theme-provider';
+import { TooltipProvider } from '@/_components/ui/tooltip';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,7 +41,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <TooltipProvider>
+            {children}
+          </TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
